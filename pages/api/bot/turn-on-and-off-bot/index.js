7a609f15893979b0d3ed21c2bb064c0b639cbdf1
Ali-Haider <alihaider15915a@gmail.com>
2023-08-18 22:10:49 +0500

    fixed new bug

--- pages/api/bot/turn-on-and-off-bot/index.js
+++ pages/api/bot/turn-on-and-off-bot/index.js
@@ -10,10 +10,21 @@ export default async function handler(req, res) {
       try {
         console.log("api-state", req.body);
         await connectMongo();
-        const strategy = await Bot.findByIdAndUpdate(id, {
+        const updatedBot = await Bot.findByIdAndUpdate(id, {
           state: req?.body,
         });
-        res.status(200).json({ status: 200, body: strategy });
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_BACKEND_URL}exchanges/single/${updatedBot?.exchange}`,
+          {
+            method: "GET",
+          }
+        );
+        const data = await response.json();
+        console.log(data);
+        let botWithExchange = updatedBot.toObject();
+        botWithExchange.exchange = data;
+        // return itemObj;
+        res.status(200).json({ status: 200, body: botWithExchange });
       } catch (error) {
         res.status(500).json({
           status: 500,
